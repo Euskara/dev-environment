fac35ffe7fda1b19941d15a4672fa98b7c15dcf7
gudari <aartola1986@gmail.com>
2017-05-30 22:44:37 +0200

    try dependencies for nifi

--- puppet/roles/manifests/nifi/master.pp
+++ puppet/roles/manifests/nifi/master.pp
@@ -3,5 +3,9 @@ class roles::nifi::master {
   include ::profiles::confs::zookeeper::standalone
   include ::profiles::confs::nifi::node
 
-  Class['::profiles::utils::java'] -> Class['::profiles::confs::zookeeper::standalone'] -> Class['::profiles::confs::nifi::node']
+  anchor { '::roles::nifi::master::start': } ->
+  Class['::profiles::utils::java'] ->
+  Class['::profiles::confs::zookeeper::standalone'] -> 
+  Class['::profiles::confs::nifi::node'] ->
+  anchor { '::roles::nifi::master::end': }
 }
--- puppet/roles/manifests/nifi/node.pp
+++ puppet/roles/manifests/nifi/node.pp
@@ -3,4 +3,9 @@ class roles::nifi::node {
   include ::profiles::utils::java
   include ::profiles::confs::nifi::node
 
+  anchor { '::roles::nifi::node::start': } ->
+  Class['::profiles::utils::java'] ->
+  Class['::profiles::confs::nifi::node'] ->
+  anchor { '::roles::nifi::node::end': }
+
 }
