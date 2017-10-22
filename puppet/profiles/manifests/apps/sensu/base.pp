258449366ecebb3736a266b0cb062ad774b9d6cd
gudari <aartola1986@gmail.com>
2017-10-23 01:38:50 +0200

    fix ipaddress facter

--- puppet/profiles/manifests/apps/sensu/base.pp
+++ puppet/profiles/manifests/apps/sensu/base.pp
@@ -4,10 +4,10 @@ class profiles::apps::sensu::base (
   class { 'sensu':
     server            => true,
     api               => true,
-    rabbitmq_host     => $ipaddress_enp0s8,
+    rabbitmq_host     => $::ipaddress_enp0s8,
     rabbitmq_user     => 'sensu',
     rabbitmq_password => 'sensu',
     rabbitmq_vhost    => '/sensu',
-    redis_host        => $ipaddress_enp0s8,
+    redis_host        => $::ipaddress_enp0s8,
   }
 }
