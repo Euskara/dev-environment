7de5d925ff043cad154a67c8dbd4e2fd5ed17c49
gudari <aartola1986@gmail.com>
2017-10-23 01:39:26 +0200

    include uchiwa in sensu standalone conf

--- puppet/roles/manifests/sensu/standalone.pp
+++ puppet/roles/manifests/sensu/standalone.pp
@@ -2,4 +2,5 @@ class roles::sensu::standalone {
   include ::profiles::confs::rabbitmq::standalone
   include ::profiles::confs::redis::standalone
   include ::profiles::confs::sensu::standalone
+  include ::profiles::confs::uchiwa::standalone
 }
