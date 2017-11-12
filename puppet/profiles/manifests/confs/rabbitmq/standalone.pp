13c835818eee01781ad1bcd6558acb496ec44bbe
gudari <aartola1986@gmail.com>
2017-11-12 03:19:10 +0100

    move user, vhost and permissions values to hiera

--- puppet/profiles/manifests/confs/rabbitmq/standalone.pp
+++ puppet/profiles/manifests/confs/rabbitmq/standalone.pp
@@ -1,40 +1,24 @@
 class profiles::confs::rabbitmq::standalone (
+
   $ensure,
   $erlang_ensure,
+  $users,
+  $vhosts,
+  $user_permissions,
 
 ){
 
   class { 'profiles::utils::erlang':
     ensure => $erlang_ensure,
   }
+
   class { 'profiles::apps::rabbitmq::base':
     ensure  => $ensure,
     require => Class[ 'profiles::utils::erlang' ]
   }
-  rabbitmq_user { 'vagrant':
-    admin    =>  true,
-    password =>  'vagrant',
-  }
-  rabbitmq_user { 'sensu':
-    password => 'sensu',
-  }
-  rabbitmq_vhost { '/sensu':
-    ensure => present,
-  }
-  rabbitmq_user_permissions { 'vagrant@/':
-    configure_permission => '.*',
-    read_permission      => '.*',
-    write_permission     => '.*',
-  }
-  rabbitmq_user_permissions { 'vagrant@/sensu':
-    configure_permission => '.*',
-    read_permission      => '.*',
-    write_permission     => '.*',
-  }
-  rabbitmq_user_permissions { 'sensu@/sensu':
-    configure_permission =>  '.*',
-    read_permission      =>  '.*',
-    write_permission     =>  '.*',
-  }
+
+  ensure_resources('rabbitmq_user', $users)
+  ensure_resources('rabbitmq_vhost', $vhosts)
+  ensure_resources('rabbitmq_user_permissions', $user_permissions)
 
 }
